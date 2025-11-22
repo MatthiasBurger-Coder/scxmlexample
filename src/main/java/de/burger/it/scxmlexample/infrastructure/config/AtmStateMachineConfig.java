25e81fbae2243793efa9f9a160a84932a9403f24
Matthias Burger <github@family-burger.de>
2025-11-22 20:59:26 +0100

    Load ATM state machine from YAML configuration

--- src/main/java/de/burger/it/scxmlexample/infrastructure/config/AtmStateMachineConfig.java
+++ src/main/java/de/burger/it/scxmlexample/infrastructure/config/AtmStateMachineConfig.java
@@ -4,41 +4,25 @@ package de.burger.it.scxmlexample.infrastructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.EnableStateMachine;
-import org.springframework.statemachine.config.StateMachineBuilder;
-import org.springframework.statemachine.config.StateMachineFactory;
-import org.springframework.statemachine.config.model.StateMachineModel;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineModelConfigurer;
 import org.springframework.statemachine.config.model.StateMachineModelFactory;
-import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.statemachine.config.model.YamlStateMachineModelFactory;
 
 @Configuration
 @EnableStateMachine
-public class AtmStateMachineConfig {
+public class AtmStateMachineConfig extends StateMachineConfigurerAdapter<String, String> {
 
-    @Bean
-    public StateMachineModelFactory<String, String> modelFactory() {
-        return new YamlStateMachineModelFactory(new ClassPathResource("application.yml"));
+    @Override
+    public void configure(StateMachineModelConfigurer<String, String> model) throws Exception {
+        model
+                .withModel()
+                .factory(modelFactory());
     }
 
     @Bean
-    public StateMachine<String, String> atmStateMachine(StateMachineModelFactory<String, String> yamlModelFactory) throws Exception {
-        // YAML in ein StateMachineModel laden
-        StateMachineModel<String, String> model = yamlModelFactory.build();
-
-        // Builder initialisieren
-        StateMachineBuilder.Builder<String, String> builder = StateMachineBuilder.builder();
-        builder.configureStates().withStates().states(model.getStatesData().getStates()).initial(model.getConfigurationData().getInitialState());
-
-        builder.configureTransitions().withExternal()
-                .and().transitions(model.getTransitionsData().getTransitions());
-
-        StateMachine<String, String> machine = builder.build();
-        machine.getStateMachineAccessor().doWithAllRegions(access ->
-                access.resetStateMachine(new DefaultStateMachineContext<>(model.getConfigurationData().getInitialState(), null, null, null))
-        );
-        machine.start();
-
-        return machine;
+    public StateMachineModelFactory<String, String> modelFactory() {
+        return new YamlStateMachineModelFactory(new ClassPathResource("atm-machine.yml"));
     }
 }
