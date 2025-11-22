25b48356312e1222db9b9363864f9f722e3355e5
Matthias Burger <github@family-burger.de>
2025-11-22 21:43:19 +0100

    Build state machine from SCXML definition

--- src/main/java/de/burger/it/scxmlexample/infrastructure/config/AtmStateMachineConfig.java
+++ src/main/java/de/burger/it/scxmlexample/infrastructure/config/AtmStateMachineConfig.java
@@ -1,44 +1,45 @@
 package de.burger.it.scxmlexample.infrastructure.config;
 
-
+import org.apache.commons.scxml2.model.SCXML;
+import org.apache.commons.scxml2.model.State;
+import org.apache.commons.scxml2.model.Transition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.StateMachineBuilder;
-import org.springframework.statemachine.config.StateMachineFactory;
-import org.springframework.statemachine.config.model.StateMachineModel;
-import org.springframework.statemachine.config.model.StateMachineModelFactory;
-import org.springframework.statemachine.support.DefaultStateMachineContext;
 
 @Configuration
-@EnableStateMachine
 public class AtmStateMachineConfig {
 
-    @Bean
-    public StateMachineModelFactory<String, String> modelFactory() {
-        return new YamlStateMachineModelFactory(new ClassPathResource("application.yml"));
-    }
+    private static final String SCXML_RESOURCE = "statemachine.scxml";
 
     @Bean
-    public StateMachine<String, String> atmStateMachine(StateMachineModelFactory<String, String> yamlModelFactory) throws Exception {
-        // YAML in ein StateMachineModel laden
-        StateMachineModel<String, String> model = yamlModelFactory.build();
+    public StateMachine<String, String> atmStateMachine() {
+        ScxmlParser parser = new ScxmlParser();
+        SCXML scxml = parser.parseScxml(SCXML_RESOURCE);
 
-        // Builder initialisieren
         StateMachineBuilder.Builder<String, String> builder = StateMachineBuilder.builder();
-        builder.configureStates().withStates().states(model.getStatesData().getStates()).initial(model.getConfigurationData().getInitialState());
 
-        builder.configureTransitions().withExternal()
-                .and().transitions(model.getTransitionsData().getTransitions());
+        builder.configureStates()
+                .withStates()
+                .initial(scxml.getInitial())
+                .states(scxml.getChildren().keySet());
 
-        StateMachine<String, String> machine = builder.build();
-        machine.getStateMachineAccessor().doWithAllRegions(access ->
-                access.resetStateMachine(new DefaultStateMachineContext<>(model.getConfigurationData().getInitialState(), null, null, null))
-        );
-        machine.start();
+        var transitions = builder.configureTransitions();
+        transitions.withExternal();
 
-        return machine;
+        for (Object child : scxml.getChildren().values()) {
+            if (child instanceof State state) {
+                for (Transition transition : state.getTransitionsList()) {
+                    transitions
+                            .withExternal()
+                            .source(state.getId())
+                            .target(transition.getTarget())
+                            .event(transition.getEvent());
+                }
+            }
+        }
+
+        return builder.build();
     }
 }
